f41bd30a7b70259a64411403c4b088744187e916
BHARGAV <54997063+bhargav4889@users.noreply.github.com>
2024-08-02 10:34:29 +0530

    Add files via upload

---
+++ ArraysBasic.java
@@ -0,0 +1,31 @@
+public class ArraysBasic {
+    // binary 
+
+    public static void main(String[] args) {
+        int[] arr = {2,4,5,6};
+
+        // int ans = Arrays.binarySearch(arr,2);
+    
+    
+        System.out.println(FindIndex(arr,1)); 
+
+    }
+
+
+    // method  find the index 
+
+    static int FindIndex(int[] arr,int num){  // array pass
+       for(int k = 0; k < arr.length - 1; k++){ // loop start
+            if(arr[k] == num){  // check 
+                return k; // index return 
+            }
+       }
+
+       return -1; // nothing then return -1
+    }
+
+
+    // Duplicate Array Logic 
+    
+
+}
